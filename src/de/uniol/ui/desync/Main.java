c4015d1f661c2dfba17621fbed5d8c8194ed7feb
hinrichs <hinrichs@f626433a-2895-11dd-a5f2-844af5208ae5>
2008-07-07 17:45:49 +0000

    new main
    git-svn-id: file:///home/chh/dump/svn/chh/Fridge@76 f626433a-2895-11dd-a5f2-844af5208ae5

---
+++ src/de/uniol/ui/desync/Main.java
@@ -0,0 +1,50 @@
+package de.uniol.ui.desync;
+
+import simkit.Schedule;
+import simkit.random.LKSeeds;
+import simkit.stat.SimpleStatsTally;
+import de.uniol.ui.desync.model.SimEntityClean;
+import de.uniol.ui.desync.util.MessagingEventList;
+
+public class Main {
+
+	private static int runs = 1;
+	
+	public static void main(String[] args) {
+		Configuration conf = new Configuration();
+		conf.showResults = true;
+		conf.variate_mc = false;
+		conf.POPULATION_SIZE = 30;
+		SimpleStatsTally sst = new SimpleStatsTally();
+		// Get FEL
+		final int list = Schedule.addNewEventList(MessagingEventList.class);
+		final MessagingEventList el = (MessagingEventList) Schedule
+				.getEventList(list);
+		Experiment exp = new Experiment(conf);
+		for (int i = 1; i <= runs; i++) {
+			conf.variate_mc_seed = LKSeeds.ZRNG[i];
+			conf.variate_Tcurrent_seed = LKSeeds.ZRNG[runs + i];
+			
+			exp.run(el);
+			
+			double mean = exp.getLoadStats().getMean();
+			System.out.println(exp.getName() + " - " + exp.getSimulationTime()
+					+ "s - mean load = " + mean);
+			sst.newObservation(mean);
+			
+			// Cleanup
+			el.reset();
+			el.coldReset();
+			exp.clear();
+			SimEntityClean.coldReset();
+			System.gc();
+		}
+		System.out.println("\nOverall stats:");
+		System.out.println("\tn        = " + sst.getCount());
+		System.out.println("\tmin      = " + sst.getMinObs());
+		System.out.println("\tmax      = " + sst.getMaxObs());
+		System.out.println("\tmean     = " + sst.getMean());
+		System.out.println("\tvariance = " + sst.getVariance());
+		System.out.println("\tstd.dev  = " + sst.getStandardDeviation());
+	}
+}
