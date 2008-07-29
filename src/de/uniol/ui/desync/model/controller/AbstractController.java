e1e6dbb57ba08fd4b042a7fc2098aa89d329ba27
hinrichs <hinrichs@f626433a-2895-11dd-a5f2-844af5208ae5>
2008-07-29 15:08:43 +0000

    added bernoulli-variate
    git-svn-id: file:///home/chh/dump/svn/chh/Fridge@137 f626433a-2895-11dd-a5f2-844af5208ae5

--- src/de/uniol/ui/desync/model/controller/AbstractController.java
+++ src/de/uniol/ui/desync/model/controller/AbstractController.java
@@ -1,5 +1,6 @@
 package de.uniol.ui.desync.model.controller;
 
+import simkit.random.BernoulliVariate;
 import simkit.random.RandomVariate;
 import simkit.random.UniformVariate;
 import de.uniol.ui.desync.model.SimEntityClean;
@@ -8,28 +9,41 @@ import de.uniol.ui.desync.model.fridges.AbstractFridge;
 public abstract class AbstractController extends SimEntityClean {
 
 	protected AbstractFridge fridge;
-	private static RandomVariate random;
+	private static RandomVariate uniform;
+	private static RandomVariate bernoulli;
 
 	private double lastLow = Double.NaN;
 	private double lastHigh = Double.NaN;
+	private double lastPropability = Double.NaN;
 
 	public AbstractController(AbstractFridge fridge, int eventListID) {
 		super(eventListID);
 		this.fridge = fridge;
 		setName(getClass().getSimpleName() + " for " + fridge.getName());
 		setEventListID(fridge.getEventListID());
-		if (random == null) {
-			random = new UniformVariate();
+		if (uniform == null) {
+			uniform = new UniformVariate();
+		}
+		if (bernoulli == null) {
+			bernoulli = new BernoulliVariate();
 		}
 	}
 
 	protected double drawUniformRandom(double low, double high) {
 		if (lastLow != low || lastHigh != high) {
-			random.setParameters(low, high);
+			uniform.setParameters(low, high);
 			lastLow = low;
 			lastHigh = high;
 		}
-		return random.generate();
+		return uniform.generate();
+	}
+	
+	protected double drawBernoulli(double propability) {
+		if (lastPropability != propability) {
+			bernoulli.setParameters(propability);
+			lastPropability = propability;
+		}
+		return bernoulli.generate();
 	}
 
 	/**
