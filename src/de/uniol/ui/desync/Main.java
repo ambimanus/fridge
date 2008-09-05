bd415c5399e26614958e8d471d288585706ebd91
hinrichs <hinrichs@f626433a-2895-11dd-a5f2-844af5208ae5>
2008-09-05 15:14:26 +0000

    corrections & settings for meanload-dauer experiments
    git-svn-id: file:///home/chh/dump/svn/chh/Fridge@167 f626433a-2895-11dd-a5f2-844af5208ae5

--- src/de/uniol/ui/desync/Main.java
+++ src/de/uniol/ui/desync/Main.java
@@ -1,32 +1,33 @@
 package de.uniol.ui.desync;
 
 import simkit.Schedule;
-import simkit.random.LKSeeds;
 import simkit.stat.SimpleStatsTally;
-import de.uniol.ui.desync.model.SimEntityClean;
 import de.uniol.ui.desync.util.MessagingEventList;
 
 public class Main {
 
-	private static int runs = 1;
+	private static int runs = 10;
 //	private static File file = new File(System.getProperty("user.dir")
 //			+ File.separator + "data" + File.separator + "out.csv");
 	
 	public static void main(String[] args) {
 		Configuration conf = new Configuration();
 		SimpleStatsTally sst = new SimpleStatsTally();
-		// Get FEL
-		final int list = Schedule.addNewEventList(MessagingEventList.class);
-		final MessagingEventList el = (MessagingEventList) Schedule
-				.getEventList(list);
-		Experiment exp = new Experiment(conf);
+		// Perform experiment(s)
 		for (int i = 1; i <= runs; i++) {
-			conf.variate_mc_seed = LKSeeds.ZRNG[i];
-			conf.variate_Tcurrent_seed = LKSeeds.ZRNG[LKSeeds.ZRNG.length - i];
+//			conf.variate_mc_seed = LKSeeds.ZRNG[i];
+//			conf.variate_Tcurrent_seed = LKSeeds.ZRNG[LKSeeds.ZRNG.length - i];
+			conf.SIMULATION_LENGTH = 0.0 + i;
+
+			// Get FEL
+			int list = Schedule.addNewEventList(MessagingEventList.class);
+			MessagingEventList el = (MessagingEventList) Schedule
+					.getEventList(list);
 			
+			Experiment exp = new Experiment(conf);
 			exp.run(el);
-			System.out.print(exp.getName() + " - " + exp.getSimulationTime()
-					+ "s");
+			System.out.print(exp.getName() + "(" + conf.SIMULATION_LENGTH
+					+ "h) - " + exp.getSimulationTime() + "s");
 			
 			if (exp.getLoadStats() != null) {
 				double mean = exp.getLoadStats().getMean();
@@ -40,10 +41,15 @@ public class Main {
 //			ResultWriter.writeResults(conf, exp, file);
 			
 			// Cleanup
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException ie) {
+			}
 			el.reset();
 			el.coldReset();
-			exp.clear();
-			SimEntityClean.coldReset();
+//			exp.clear();
+//			SimEntityClean.coldReset();
+			exp = null;
 			System.gc();
 		}
 		System.out.println("\nOverall stats:");
