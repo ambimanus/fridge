9f5b7f3ac00eea1e79b7b8c0021e6512091464da
hinrichs <hinrichs@f626433a-2895-11dd-a5f2-844af5208ae5>
2008-11-20 11:38:58 +0000

    added license information
    git-svn-id: file:///home/chh/dump/svn/chh/Fridge@259 f626433a-2895-11dd-a5f2-844af5208ae5

--- src/de/uniol/ui/desync/model/SimEntityClean.java
+++ src/de/uniol/ui/desync/model/SimEntityClean.java
@@ -1,3 +1,27 @@
+/*
+ * AdaptiveFridge Copyright (C) 2008 Christian Hinrichs
+ * 
+ * AdaptiveFridge is copyright under the GNU General Public License.
+ * 
+ * This file is part of AdaptiveFridge.
+ * 
+ * AdaptiveFridge is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * AdaptiveFridge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with AdaptiveFridge.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * NOTICE:
+ * This file is a modified version of simkit.SimEntityBase from Simkit.
+ * Simkit Copyright (C) 1997-2007 Kirk Stork and Arnold Buss
+ */
 package de.uniol.ui.desync.model;
 
 import java.lang.reflect.InvocationTargetException;
@@ -14,6 +38,7 @@ import simkit.SimEntityBase;
 import simkit.SimEvent;
 import simkit.Stop;
 import simkit.util.LinkedHashMap2;
+
 
 /**
  *  Default implementation of a SimEntity using reflection.  Consequently, the
